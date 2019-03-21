d9b6cf8f33be358d3b0552d64d59f2372b7dd15e
hanna <hannamaria44@gmail.com>
2019-03-21 18:52:51 -0500

    I created the form view

--- js/quote_tool.js
+++ js/quote_tool.js
@@ -1,1 +1,11 @@
 // Qouting tool logic should be placed here.
+const inputs = document.getElementsByClassName('input_form');
+for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener('keyup', function() {
+        if (this.value.length >= 1) {
+            this.nextElementSibling.classList.add('fix_text');
+        } else {
+            this.nextElementSibling.classList.remove('fix_text');
+        }
+    });
+}
