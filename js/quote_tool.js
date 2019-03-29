cced67f6ef05dc8ed8749c687855076c99afc444
frankruiz90 <frankruiz_90@hotmail.com>
2019-03-28 19:02:54 -0500

    modify the if and create a constant file

--- js/quote_tool.js
+++ js/quote_tool.js
@@ -1,66 +1,64 @@
 // Qouting tool logic should be placed here.
-//they contain the cost of the night and the cost of transportation by boat and bus.
 
-const PRICENIGTH = 55000;
-const BUSTRANS = 75000;
-const BOATTRANS = 75000;
-// Tour aguacate, playa soledad
-const TOURAPS = 65000;
-// Tour sazurro, la miel
-const TOURSLM = 60000;
-// Hike el cielo, el trebol
-const HIKEECET = 40000;
-// Hike el paraiso
-const HIKEP = 30000;
-//Hike la coquerita
-const HIKECO = 5000;
-// guide hike la coquerita
-const HIKEGUI = 10000;
-//Mandatory insurance cost per night per person.
-const INSURANCE = 3500;
+// Remember to import consts.js if you will use this file 
 
-/*calculateCost calculates the value of the package that the user prefers 
-*by giving a value of each one of the options selected by the user
-*/
+/* calculateCost calculates the value of the package that the user prefers 
+ * by giving a value of each one of the options selected by the user
+ * 
+ * The expected objet structure is :
+ * {
+ *  numberOfPeople: integer,
+ *  numberOfNigth: integer,
+ *  trasnport: object, 
+ *  tour: object,
+ *  hike: object,
+ * } 
+ * 
+ * trasnport: {
+ *  medNec: boolean,
+ *  necCap: boolean,
+ *  capNec: boolean,
+ *  necMed: boolean,
+ * }
+ * tour: {
+ *  aguacatePlayasoledad: boolean,
+ *  sapzurroLamiel: boolean,
+ * }
+ * hike: {
+ *  elCieloEltrebol: boolean,
+ *  elParaiso: boolean,
+ *  laCoquerita: object,
+ * }
+ * laCoquerita: {
+ *  hike: boolean,
+ *  guide: boolean,
+ * }
+ */
 function calculateCost(){
     let total = 0;
+    // PeopleAndNigth It contains the number of people multiplied the number of 
+    //nights and then multiply the result by the heat of each night.
     let peopleAndNigth;
-    if (form.numberOfPeople > 0){
-        peopleAndNigth =  (form.numberOfPeople * form.numberOfNigth);
+    
+    peopleAndNigth =  (form.numberOfPeople * form.numberOfNigth); 
+    total += (PRICENIGTH* peopleAndNigth );
+    
+    total += form.transport.medNec ? (BUSTRANS * form.numberOfPeople) : 0;
+    total += form.transport.necCap ? (BOATTRANS * form.numberOfPeople) : 0;
+    total += form.transport.capNec ? (BOATTRANS * form.numberOfPeople) : 0;
+    total += form.transport.necMed ? (BUSTRANS * form.numberOfPeople) : 0;
+    
+    total += form.tour.aguacatePlayasoledad ? (TOURAPS * form.numberOfPeople) : 0;
+    total += form.tour.sapzurroLamiel ? (TOURSLM * form.numberOfPeople) : 0;
+    
+    total += form.hike.elCieloEltrebol ? (HIKEECET * form.numberOfPeople) : 0;
+    total += form.hike.elParaiso ? (HIKEP * form.numberOfPeople) : 0;
+      if (from.hike.laCoquerita.hike){
+      total += HIKECO * form.numberOfPeople;
+      if (form.hike.laCoquerita.guide){
+          total +=  HIKEGUI * form.numberOfPeople;
+      }
     }
-    if (form.numberOfNigth > 0){
-        total = total + (PRICENIGTH* aux );
-    }
-    if (form.transport.medNec){
-        total = total + (BUSTRANS * form.numberOfPeople);
-    }
-    if (form.transport.necCap){
-        total = total + (BOATTRANS * form.numberOfPeople);
-    }
-    if (form.transport.capNec){
-        total = total + (BOATTRANS * form.numberOfPeople);
-    }
-    if (form.transport.necMed){
-        totla = total + (BUSTRANS * form.numberOfPeople);
-    }
-    if (form.tour.aguacatePlayasoledad){
-        total = total + (TOURAPS * form.numberOfPeople);
-    }
-    if (form.hike.sapzurroLamiel){
-        total = total + (TOURSLM * form.numberOfPeople);
-    }
-    if (form.hike.elCieloEltrebol){
-        total = total + (HIKEECET * form.numberOfPeople);
-    }
-    if (form.hike.elParaiso){
-        total = total + (HIKEP * form.numberOfPeople);
-    }
-    if (form.hike.laCoquerita.hike && form.hike.laCoquerita.guide){
-        total = total + ((HIKECO + HIKEGUI) * form.numberOfPeople);
-    }
-    else{
-        total = total + (HIKECO);
-    }
-     total = total + (INSURANCE * peopleAndNigth);
-     return total;
+    total += (INSURANCE * peopleAndNigth);
+    return total;
 }
