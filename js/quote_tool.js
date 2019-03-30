58d34712a28ca18ecbf84ae436585ec7510919c0
Juan Ramirez <juskhc@gmail.com>
2019-03-29 21:03:01 -0500

    Indentation fixed

--- js/quote_tool.js
+++ js/quote_tool.js
@@ -2,16 +2,16 @@
  * This is a validation of labels; if in the inputs there are one or more characters,
  * the label should stay in the position assigned by the class "fix-label".
  */
-let inputs = document.getElementsByClassName('form__input');
-const fixLabel = (inputs) => {
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener('keyup', function() {
-            if (this.value.length >= 1) {
-                this.nextElementSibling.classList.add('fix-label');
-            } else {
-                this.nextElementSibling.classList.remove('fix-label');
-            }
-        });
-    }
-}
+let inputs = document.getElementsByClassName("form__input");
+const fixLabel = inputs => {
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("keyup", function() {
+      if (this.value.length >= 1) {
+        this.nextElementSibling.classList.add("fix-label");
+      } else {
+        this.nextElementSibling.classList.remove("fix-label");
+      }
+    });
+  }
+};
 fixLabel(inputs);
