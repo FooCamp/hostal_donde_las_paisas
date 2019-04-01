fa07cb97ef2807d9332dcf84330bd6f1d7e97a03
Victor <draimien424@gmail.com>
2019-04-01 15:01:05 -0500

    arreglos de el carrousel

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-/* Logic for page animations and simple interactions should be placed here
-   Any page or component specific logic should be placed in its own page/component js file.
-**/
-
-
-/*  Commodities section
-    from line 7 to 43 is about the Commodities image on the main page **/
 let image = ['assets/img/cat1.jpg',
              'assets/img/cat2.png', 
              'assets/img/cat3.jpeg',
@@ -40,7 +33,6 @@ function carrousel(container){
             }
     });
 }
-
 let container = document.querySelector('.container');
 console.log(container);
 carrousel(container);
