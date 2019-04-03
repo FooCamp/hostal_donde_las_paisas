16fcf18fb238d8b5161f64bfdfa3639cc1d1e321
Sonny97 <draimien424@gmail.com>
2019-04-03 15:56:33 -0500

    aplicado la opcion format document y cambiado nombres de clases y variables sugeridas

--- js/index.js
+++ js/index.js
@@ -1,41 +1,39 @@
 /* Logic for page animations and simple interactions should be placed here
    Any page or component specific logic should be placed in its own page/component js file.
 **/
-let image = ['assets/img/cat1.jpg',
-             'assets/img/cat2.png', 
-             'assets/img/cat3.jpeg',
-             'assets/img/momia.jpeg'],
-    cont = 0;
+let image = [
+    "assets/img/cat1.jpg",
+    "assets/img/cat2.png",
+    "assets/img/cat3.jpeg",
+    "assets/img/momia.jpeg"
+  ],
+  currentPosition = 0;
 
-function carrousel(container){
-    container.addEventListener('click', e =>{
-        let back = container.querySelector('.back'),
-            ahead = container.querySelector('.ahead'),
-            img = container.querySelector('img'),
-            target = e.target;
+function carrousel(carrouselContainer) {
+  carrouselContainer.addEventListener("click", e => {
+    let back = carrouselContainer.querySelector(".back"),
+      ahead = carrouselContainer.querySelector(".ahead"),
+      img = carrouselContainer.querySelector("img"),
+      target = e.target;
 
     if (target == back) {
-        if (cont > 0) {
-            img.src = image [cont-1];
-            cont--;
-        }
-        else{
-            img.src = image[image.length-1];
-            cont = image.length - 1;
-        }
-        
-    } else if(target == ahead){
-        if (cont < image.length-1) {
-            img.src = image[cont+1];
-            cont++;
-        }
-        else{
-            img.src = image[0];
-            cont = 0;
-        }
+      if (currentPosition > 0) {
+        img.src = image[currentPosition - 1];
+        currentPosition--;
+      } else {
+        img.src = image[image.length - 1];
+        currentPosition = image.length - 1;
+      }
+    } else if (target == ahead) {
+      if (currentPosition < image.length - 1) {
+        img.src = image[currentPosition + 1];
+        currentPosition++;
+      } else {
+        img.src = image[0];
+        currentPosition = 0;
+      }
     }
-    });
+  });
 }
-let container = document.querySelector('.container');
-console.log(container);
-carrousel(container);
+let carrouselContainer = document.querySelector(".carrouselContainer");
+carrousel(carrouselContainer);
