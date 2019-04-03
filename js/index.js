5e128dde0b3f306d6a2831d1aeb168f10acee898
Sonny97 <draimien424@gmail.com>
2019-04-03 15:32:49 -0500

    inluido los comentarios originales en los archivos de quote y home, tambien en js los de quote_tools y gallery

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+/* Logic for page animations and simple interactions should be placed here
+   Any page or component specific logic should be placed in its own page/component js file.
+**/
 let image = ['assets/img/cat1.jpg',
              'assets/img/cat2.png', 
              'assets/img/cat3.jpeg',
@@ -11,26 +14,26 @@ function carrousel(container){
             img = container.querySelector('img'),
             target = e.target;
 
-            if (target == back) {
-                if (cont > 0) {
-                    img.src = image [cont-1];
-                    cont--;
-                }
-                else{
-                    img.src = image[image.length-1];
-                    cont = image.length - 1;
-                }
-                
-            } else if(target == ahead){
-                if (cont < image.length-1) {
-                    img.src = image[cont+1];
-                    cont++;
-                }
-                else{
-                    img.src = image[0];
-                    cont = 0;
-                }
-            }
+    if (target == back) {
+        if (cont > 0) {
+            img.src = image [cont-1];
+            cont--;
+        }
+        else{
+            img.src = image[image.length-1];
+            cont = image.length - 1;
+        }
+        
+    } else if(target == ahead){
+        if (cont < image.length-1) {
+            img.src = image[cont+1];
+            cont++;
+        }
+        else{
+            img.src = image[0];
+            cont = 0;
+        }
+    }
     });
 }
 let container = document.querySelector('.container');
---
+++ js/quote_tool.js
@@ -0,0 +1,4 @@
+/*
+ * This is a validation of labels; if in the inputs there are one or more characters,
+ * the label should stay in the position assigned by the class "fix-label".
+ */
