b8a4cecafc9818c8aa1d76b7359e311c704ec217
Sonny97 <draimien424@gmail.com>
2019-04-10 12:54:36 -0500

    agree the image for the carousel, also paste the cod about comodities carousel section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,39 @@
 /* Logic for page animations and simple interactions should be placed here
    Any page or component specific logic should be placed in its own page/component js file.
 **/
+let image = [
+  "assets/img/IMG_8056.jpg",
+  "assets/img/IMG_8453.jpg",
+  "assets/img/IMG_7977.jpg",
+  "assets/img/IMG_7968.jpg"
+],
+currentPosition = 0;
+
+function carrousel(carrouselContainer) {
+carrouselContainer.addEventListener("click", e => {
+  let back = carrouselContainer.querySelector(".back"),
+    ahead = carrouselContainer.querySelector(".ahead"),
+    img = carrouselContainer.querySelector("img"),
+    target = e.target;
+
+  if (target == back) {
+    if (currentPosition > 0) {
+      img.src = image[currentPosition - 1];
+      currentPosition--;
+    } else {
+      img.src = image[image.length - 1];
+      currentPosition = image.length - 1;
+    }
+  } else if (target == ahead) {
+    if (currentPosition < image.length - 1) {
+      img.src = image[currentPosition + 1];
+      currentPosition++;
+    } else {
+      img.src = image[0];
+      currentPosition = 0;
+    }
+  }
+});
+}
+let carrouselContainer = document.querySelector(".carrouselContainer");
+carrousel(carrouselContainer);
