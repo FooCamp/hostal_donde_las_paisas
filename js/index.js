408fc5836731807b1d26d2451732d3490284d713
Victor Manuel Florez Taborda <draimien424@gmail.com>
2019-04-17 12:46:28 -0500

    indented code

--- js/index.js
+++ js/index.js
@@ -1,41 +1,41 @@
 /* Logic for page animations and simple interactions should be placed here
-Any page or component specific logic should be placed in its own page/component js file.
+   Any page or component specific logic should be placed in its own page/component js file.
 **/
 /*
-this function validates the position of the image in the array to know if it returns to the previous one or goes forward,
-also if it is in the position [0] and returns it to the last position [3] also to the reverse
+  this function validates the position of the image in the array to know if it returns to the previous one or goes forward,
+   also if it is in the position [0] and returns it to the last position [3] also to the reverse
 */
 function carrousel() {
-switch (this.dataset.direction) {
-case "back":
-if (currentPosition > 0) {
-carrouselImage.src = image[currentPosition - 1];
-currentPosition--;
-} else {
-carrouselImage.src = image[image.length - 1];
-currentPosition = image.length - 1;
-}
-break;
-case "ahead":
-if (currentPosition < image.length - 1) {
-carrouselImage.src = image[currentPosition + 1];
-currentPosition++;
-} else {
-carrouselImage.src = image[0];
-currentPosition = 0;
-}
-break;
-default:
-break;
-}
+  switch (this.dataset.direction) {
+    case "back":
+      if (currentPosition > 0) {
+        carrouselImage.src = image[currentPosition - 1];
+        currentPosition--;
+      } else {
+        carrouselImage.src = image[image.length - 1];
+        currentPosition = image.length - 1;
+      }
+      break;
+    case "ahead":
+      if (currentPosition < image.length - 1) {
+        carrouselImage.src = image[currentPosition + 1];
+        currentPosition++;
+      } else {
+        carrouselImage.src = image[0];
+        currentPosition = 0;
+      }
+      break;
+    default:
+      break;
+  }
 }
 /*Array about images */
 const image = [
-"assets/images/IMG_8056.JPG",
-"assets/images/IMG_8453.JPG",
-"assets/images/IMG_7977.JPG",
-"assets/images/IMG_7968.JPG"
-];
+  "assets/images/IMG_8056.JPG",
+  "assets/images/IMG_8453.JPG",
+  "assets/images/IMG_7977.JPG",
+  "assets/images/IMG_7968.JPG"
+  ];
 /*count the positions of the array to know what is the previus or next depend of button*/
 let currentPosition = 0;
 /*Take all arrows in the document*/
@@ -44,5 +44,5 @@ const carrouselArrows = document.querySelectorAll(".carrousel__arrow");
 const carrouselImage = document.querySelector(".carrousel__img");
 /*In all elements of document, take all carrousel__arrow to send the function carrousel */
 carrouselArrows.forEach(function(element) {
-element.addEventListener("click", carrousel);
+  element.addEventListener("click", carrousel);
 });
