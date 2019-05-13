82cabe4c6ebe644dd8409d85fb609647f89ac466
Sonny97 <draimien424@gmail.com>
2019-05-13 11:01:02 -0500

    carrousel without points and button galery, also erase the classes in css/style, and js/index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 /* Logic for page animations and simple interactions should be placed here
    Any page or component specific logic should be placed in its own page/component js file.
 **/
+/*
+  this function validates the position of the image in the array to know if it returns to the previous one or goes forward,
+   also if it is in the position [0] and returns it to the last position [3] also to the reverse
+*/
 function carrousel() {
   switch (this.dataset.direction) {
     case "back":
@@ -25,15 +29,20 @@ function carrousel() {
       break;
   }
 }
+/*Array about images */
 const image = [
   "assets/images/IMG_8056.JPG",
   "assets/images/IMG_8453.JPG",
   "assets/images/IMG_7977.JPG",
   "assets/images/IMG_7968.JPG"
   ];
+/*count the positions of the array to know what is the previus or next depend of button*/
 let currentPosition = 0;
+/*Take all arrows in the document*/
 const carrouselArrows = document.querySelectorAll(".carrousel__arrow");
-const carrouselImage = carrouselContainer.querySelector(".carrousel__img");
+/* Take actual image from document */
+const carrouselImage = document.querySelector(".carrousel__img");
+/*In all elements of document, take all carrousel__arrow to send the function carrousel */
 carrouselArrows.forEach(function(element) {
   element.addEventListener("click", carrousel);
 });
