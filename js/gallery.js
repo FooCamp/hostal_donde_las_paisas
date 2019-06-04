3aa65c8f7837f5ff1f1f0e56578081909ac1dc27
Frankruiz90 <47303666+Frankruiz90@users.noreply.github.com>
2019-06-04 14:36:59 -0500

    create a function for the images

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,19 @@
 // Gallery page logic should be placed here.
+/*charge the images with backgroundImage */
+window.onload = function chargeImages()
+{
+const images = [
+    "./../assets/images/1.jpg",
+    "./../assets/images/2.jpg",
+    "./../assets/images/3.jpg",
+    "./../assets/images/4.jpg",
+    "./../assets/images/5.jpg",
+    "./../assets/images/6.jpg",
+    "./../assets/images/7.jpg",
+    "./../assets/images/8.jpg"
+]
+let div1 = document.getElementById('divImagesContainer').children;
+for (let i = 0; i < div1.length; i++){
+    div1[i].firstChild.style.backgroundImage = `url('${images[i]}')`;  
+}
+}
