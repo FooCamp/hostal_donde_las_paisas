b0b4fdbee25d47229741199c6f4f54ed086b352d
Jhonatan Rojas <jonatanrojasbastidas@gmail.com>
2019-07-22 16:02:11 -0500

    feature/ Gallery modal

--- js/gallery.js
+++ js/gallery.js
@@ -12,9 +12,43 @@ window.onload = function chargeImages() {
     "./../assets/images/gallery/8.jpg"
   ];
   let div1 = document.getElementById("divImagesContainer").children;
-  for (let i = 0; i < div1.length; i++) {
-    div1[i].firstElementChild.style.backgroundImage = `url('${
-      imagesGallery[i]
-    }')`;
+  let imagesContainer = document.querySelector('#divImagesContainer');
+
+  for (let i = 0; i < imagesGallery.length; i++) {
+    imagesContainer.innerHTML += `
+  <div class="gallery-item">
+    <div class="gallery-item__images" style="background-image: url(${imagesGallery[i]})"></div>
+    <div class="gallery-item__overlay">
+      <img
+        src="./../assets/images/gallery/expand_icon.svg"
+        class="overlay__image"
+        alt=""
+      />
+    </div>
+  </div>`;
   }
+  addListernersToGalleryItems();
+  closeModal();
 };
+
+function closeModal() {
+  let closeModal = document.querySelector('.modal__close');
+  let galleryModal = document.querySelector('#modal');
+
+  closeModal.addEventListener('click', function () {
+    galleryModal.classList.remove('gallery__modal--visible');
+  })
+}
+
+function addListernersToGalleryItems() {
+  let galleryModal = document.querySelector('#modal');
+  let galleryItems = document.querySelectorAll('.gallery-item');
+  let modalImage = document.querySelector('.modal__image');
+
+  for (const galleryItem of galleryItems) {
+    galleryItem.addEventListener('click', function () {
+      galleryModal.classList.add('gallery__modal--visible');
+      modalImage.style.backgroundImage = this.querySelector('.gallery-item__images').style.backgroundImage
+    })
+  }
+}
