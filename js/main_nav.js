6d689076a6b8a35ff0f46bbbcc5ae0edd0f08bc1
frankruiz90 <frankruiz_90@hotmail.com>
2019-08-13 21:32:10 -0500

    Format in the code

--- js/main_nav.js
+++ js/main_nav.js
@@ -21,17 +21,17 @@ if (logoElement) {
   logoScrollOffset = logoHeight + logoOffsetTop;
 
   /*
- * This function recalculates the values of logo's height and
- * the space between logo and the very top of the viewport
- * when a user makes resize of the window
- */
-window.addEventListener("resize", function() {
-  logoHeight = logoElement.offsetHeight;
-  logoOffsetTop = logoElement.offsetTop;
-  window.scrollY >= logoHeight + logoOffsetTop
-    ? overlay.classList.add("main-nav--colored")
-    : overlay.classList.remove("main-nav--colored");
-});
+   * This function recalculates the values of logo's height and
+   * the space between logo and the very top of the viewport
+   * when a user makes resize of the window
+   */
+  window.addEventListener("resize", function() {
+    logoHeight = logoElement.offsetHeight;
+    logoOffsetTop = logoElement.offsetTop;
+    window.scrollY >= logoHeight + logoOffsetTop
+      ? overlay.classList.add("main-nav--colored")
+      : overlay.classList.remove("main-nav--colored");
+  });
 }
 
 navBurguer.addEventListener("click", function() {
@@ -44,8 +44,6 @@ navBurguer.addEventListener("click", function() {
     overlay.classList.toggle("main-nav--colored");
   }
 });
-
-
 
 /*
  *This function makes the navbar's logo appears
